a18478fba25cae7bffb25b14637ca7ba8eecb400
Emmanuel Philip <emmanuelrajuphilip@gmail.com>
2019-07-03 16:06:54 +0530

    Refctor code to calculate totals and reformat code

--- src/main/java/com/movierental/Customer.java
+++ src/main/java/com/movierental/Customer.java
@@ -4,42 +4,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Customer {
-  private String name;
-  private List<Rental> rentals = new ArrayList<>();
+    private String name;
+    private List<Rental> rentals = new ArrayList<>();
 
-  public Customer(String name) {
-    this.name = name;
-  }
-
-  public void addRental(Rental arg) {
-    rentals.add(arg);
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public String statement() {
-    double totalAmount = 0;
-    int frequentRenterPoints = 0;
-    String result = "Rental Record for " + getName() + "\n";
-    for (Rental each : rentals) {
-      double thisAmount = each.amount();
-      // add frequent renter points
-      frequentRenterPoints += each.frequentRenterPoints();
-
-      //show figures for this rental
-      result += "\t" + each.getMovie().getTitle() + "\t" +
-              thisAmount + "\n";
-      totalAmount += thisAmount;
+    public Customer(String name) {
+        this.name = name;
     }
 
-    //add footer lines result
-    result += "Amount owed is " + totalAmount + "\n";
-    result += "You earned " + frequentRenterPoints
-        + " frequent renter points";
-    return result;
-  }
+    public void addRental(Rental arg) {
+        rentals.add(arg);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String statement() {
+        String result = "Rental Record for " + getName() + "\n";
+        for (Rental each : rentals) {
+            //show figures for this rental
+            result += "\t" + each.getMovie().getTitle() + "\t" +
+                    each.amount() + "\n";
+        }
+        //add footer lines result
+        result += "Amount owed is " + totalAmount() + "\n";
+        result += "You earned " + frequentRenterPoints()
+                + " frequent renter points";
+        return result;
+    }
+
+    private int frequentRenterPoints() {
+        int frequentRenterPoints = 0;
+        for (Rental each : rentals) {
+            // add frequent renter points
+            frequentRenterPoints += each.frequentRenterPoints();
+        }
+        return frequentRenterPoints;
+    }
+
+    private double totalAmount() {
+        double totalAmount = 0;
+        for (Rental each : rentals) {
+            totalAmount += each.amount();
+        }
+        return totalAmount;
+    }
 
 }
 
--- src/test/java/com/movierental/CustomerTest.java
+++ src/test/java/com/movierental/CustomerTest.java
@@ -6,20 +6,20 @@ import static org.junit.Assert.assertEquals;
 
 public class CustomerTest {
     @Test
-    public void shouldGenerateStatementBasic(){
+    public void shouldGenerateStatementBasic() {
         Customer testCustomer = new Customer("Emmanuel");
         testCustomer.addRental(new Rental(new Movie("Star Wars", 1), 2));
         testCustomer.addRental(new Rental(new Movie("Star Trek", 0), 1));
 
         assertEquals("Rental Record for Emmanuel\n" +
-        "\tStar Wars\t6.0\n" +
-        "\tStar Trek\t2.0\n" +
-        "Amount owed is 8.0\n" +
-        "You earned 3 frequent renter points", testCustomer.statement());
+                "\tStar Wars\t6.0\n" +
+                "\tStar Trek\t2.0\n" +
+                "Amount owed is 8.0\n" +
+                "You earned 3 frequent renter points", testCustomer.statement());
     }
 
     @Test
-    public void shouldGenerateStatementRegularMovieDaysRentedNotGreaterThanTwo(){
+    public void shouldGenerateStatementRegularMovieDaysRentedNotGreaterThanTwo() {
         Customer testCustomer = new Customer("Emmanuel");
         testCustomer.addRental(new Rental(new Movie("Star Wars", 0), 2));
 
@@ -30,7 +30,7 @@ public class CustomerTest {
     }
 
     @Test
-    public void shouldGenerateStatementRegularMovieDaysRentedGreaterThanTwo(){
+    public void shouldGenerateStatementRegularMovieDaysRentedGreaterThanTwo() {
         Customer testCustomer = new Customer("Emmanuel");
         testCustomer.addRental(new Rental(new Movie("Star Wars", 0), 3));
 
@@ -42,7 +42,7 @@ public class CustomerTest {
     }
 
     @Test
-    public void shouldGenerateStatementNewReleaseMovieDaysRentedNotGreaterThanOne(){
+    public void shouldGenerateStatementNewReleaseMovieDaysRentedNotGreaterThanOne() {
         Customer testCustomer = new Customer("Emmanuel");
         testCustomer.addRental(new Rental(new Movie("Star Wars", 1), 1));
 
@@ -53,7 +53,7 @@ public class CustomerTest {
     }
 
     @Test
-    public void shouldGenerateStatementNewReleaseMovieDaysRentedGreaterThanOne(){
+    public void shouldGenerateStatementNewReleaseMovieDaysRentedGreaterThanOne() {
         Customer testCustomer = new Customer("Emmanuel");
         testCustomer.addRental(new Rental(new Movie("Star Wars", 1), 2));
 
@@ -64,7 +64,7 @@ public class CustomerTest {
     }
 
     @Test
-    public void shouldGenerateStatementChildrensMovieDaysRentedNotGreaterThanThree(){
+    public void shouldGenerateStatementChildrensMovieDaysRentedNotGreaterThanThree() {
         Customer testCustomer = new Customer("Emmanuel");
         testCustomer.addRental(new Rental(new Movie("Toy Story", 2), 1));
 
@@ -75,7 +75,7 @@ public class CustomerTest {
     }
 
     @Test
-    public void shouldGenerateStatementChildrensMovieDaysRentedGreaterThanThree(){
+    public void shouldGenerateStatementChildrensMovieDaysRentedGreaterThanThree() {
         Customer testCustomer = new Customer("Emmanuel");
         testCustomer.addRental(new Rental(new Movie("Toy Story", 2), 4));
 
@@ -86,7 +86,7 @@ public class CustomerTest {
     }
 
     @Test
-    public void shouldGenerateStatementUnknownPriceCodeMovie(){
+    public void shouldGenerateStatementUnknownPriceCodeMovie() {
         Customer testCustomer = new Customer("Emmanuel");
         testCustomer.addRental(new Rental(new Movie("Fake Movie", 3), 4));
 
@@ -97,7 +97,7 @@ public class CustomerTest {
     }
 
     @Test
-    public void shouldGenerateStatementNoRentals(){
+    public void shouldGenerateStatementNoRentals() {
         Customer testCustomer = new Customer("Emmanuel");
 
         assertEquals("Rental Record for Emmanuel\n" +
