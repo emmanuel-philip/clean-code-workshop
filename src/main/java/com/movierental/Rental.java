4a9a35790daa2c201b5f18ec481bca14e06348c7
Emmanuel Philip <emmanuelrajuphilip@gmail.com>
2019-07-03 14:18:07 +0530

    Extract method to calculate frequent renter points

--- src/main/java/com/movierental/Rental.java
+++ src/main/java/com/movierental/Rental.java
@@ -38,4 +38,12 @@ public class Rental {
     }
     return thisAmount;
   }
+
+  int frequentRenterPoints() {
+    // add bonus for a two day new release rental
+    if ((getMovie().getPriceCode() == Movie.NEW_RELEASE)
+        &&
+        getDaysRented() > 1) return 2;
+    return 1;
+  }
 }
