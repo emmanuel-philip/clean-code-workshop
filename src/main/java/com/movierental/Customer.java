f54f3877dd64e6b22f42da42838cb361408bf30f
Emmanuel Philip <emmanuelrajuphilip@gmail.com>
2019-07-03 14:04:15 +0530

    Extract method to calculate amount

--- src/main/java/com/movierental/Customer.java
+++ src/main/java/com/movierental/Customer.java
@@ -24,23 +24,7 @@ public class Customer {
     int frequentRenterPoints = 0;
     String result = "Rental Record for " + getName() + "\n";
     for (Rental each : rentals) {
-      double thisAmount = 0;
-      //determine amounts for each line
-      switch (each.getMovie().getPriceCode()) {
-        case Movie.REGULAR:
-          thisAmount += 2;
-          if (each.getDaysRented() > 2)
-            thisAmount += (each.getDaysRented() - 2) * 1.5;
-          break;
-        case Movie.NEW_RELEASE:
-          thisAmount += each.getDaysRented() * 3;
-          break;
-        case Movie.CHILDRENS:
-          thisAmount += 1.5;
-          if (each.getDaysRented() > 3)
-            thisAmount += (each.getDaysRented() - 3) * 1.5;
-          break;
-      }
+      double thisAmount = each.amount();
       // add frequent renter points
       frequentRenterPoints++;
       // add bonus for a two day new release rental
@@ -50,15 +34,16 @@ public class Customer {
 
       //show figures for this rental
       result += "\t" + each.getMovie().getTitle() + "\t" +
-          String.valueOf(thisAmount) + "\n";
+              thisAmount + "\n";
       totalAmount += thisAmount;
     }
 
     //add footer lines result
-    result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-    result += "You earned " + String.valueOf(frequentRenterPoints)
+    result += "Amount owed is " + totalAmount + "\n";
+    result += "You earned " + frequentRenterPoints
         + " frequent renter points";
     return result;
   }
+
 }
 
